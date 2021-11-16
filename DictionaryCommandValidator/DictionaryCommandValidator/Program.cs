6cfd27544b67516a0b3348bc3e1519051448237c
Inavrocky <ivnavrotskiy@yandex.ru>
2021-11-16 10:07:36 +0300

    initial commit

---
+++ DictionaryCommandValidator/DictionaryCommandValidator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DictionaryCommandValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
