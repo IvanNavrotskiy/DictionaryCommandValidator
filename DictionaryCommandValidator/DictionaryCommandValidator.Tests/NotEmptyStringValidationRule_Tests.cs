ee205eaaad4789b99d1fff6af81d69e199d4231c
Иван Навроцкий <ivnavrotskiy@yandex.ru>
2021-12-20 16:45:05 +0300

    +not empty string test

--- DictionaryCommandValidator/DictionaryCommandValidator.Tests/NotEmptyStringValidationRule_Tests.cs
+++ DictionaryCommandValidator/DictionaryCommandValidator.Tests/NotEmptyStringValidationRule_Tests.cs
@@ -106,5 +106,77 @@ namespace DictionaryCommandValidator.Tests
             var property = ValidationProperty.NotEmptyString("properties.second.third.fourth.fifth.name");
             ValidationResult.ValidationSuccess_SingleProperty(command, property);
         }
+
+        [Test]
+        public void FiveNestingLevel_InnerArray_ValidationSuccess()
+        {
+            var command = new Dictionary<string, object>()
+            {
+                ["user"] = "admin",
+                ["controllerId"] = 1,
+                ["properties"] = new Dictionary<string, object>()
+                {
+                    ["second"] = new Dictionary<string, object>()
+                    {
+                        // array
+                        ["third"] = new Dictionary<string, object>[]
+                        {
+                            new Dictionary<string, object>()
+                            {
+                                ["fourth"] = new Dictionary<string,object>[]
+                                {
+                                    new Dictionary<string, object>() 
+                                    {
+                                        ["fifth"] = new Dictionary<string, object>()
+                                        {
+                                            ["name"] = "commandName"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var property = ValidationProperty.NotEmptyString("properties.second.third.fourth.fifth.name");
+            ValidationResult.ValidationSuccess_SingleProperty(command, property);
+        }
+
+        [Test]
+        public void FiveNestingLevel_InnerArray_ValidationFailure_NotString()
+        {
+            var command = new Dictionary<string, object>()
+            {
+                ["user"] = "admin",
+                ["controllerId"] = 1,
+                ["properties"] = new Dictionary<string, object>()
+                {
+                    ["second"] = new Dictionary<string, object>()
+                    {
+                        // array
+                        ["third"] = new Dictionary<string, object>[]
+                        {
+                            new Dictionary<string, object>()
+                            {
+                                ["fourth"] = new Dictionary<string,object>[]
+                                {
+                                    new Dictionary<string, object>()
+                                    {
+                                        ["fifth"] = new Dictionary<string, object>()
+                                        {
+                                            ["name"] = 123
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var property = ValidationProperty.NotEmptyString("properties.second.third.fourth.fifth.name");
+            ValidationResult.ValidationFailure_SingleProperty(command, property, "properties.second.third.fourth.fifth.name is not string");
+        }
     }
 }
